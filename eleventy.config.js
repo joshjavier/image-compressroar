830c0893d001fdab393201ec8dfa37f741a4cdf2
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-07-01 17:46:50 +0800

    Set up 11ty for base tooling

---
+++ eleventy.config.js
@@ -0,0 +1,9 @@
+/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
+export default function (eleventyConfig) {
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist'
+    }
+  }
+}
