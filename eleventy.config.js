04a6d1fa98015643fd6f06a9076af18974001929
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-07-02 13:45:12 +0800

    Set up JS bundling with esbuild

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,11 @@
+import esbuild from "./config/esbuild.js"
 import lightningcss from "./config/lightningcss.js"
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'public': '/' })
 
+  eleventyConfig.addPlugin(esbuild)
   eleventyConfig.addPlugin(lightningcss)
 
   return {
