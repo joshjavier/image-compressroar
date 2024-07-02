2ab5c3b0e183ccc086e3c8cafc06cc19643dccd3
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-07-02 12:14:57 +0800

    Set up passthrough copy and add favicon.ico

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,7 @@
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ 'public': '/' })
+
   return {
     dir: {
       input: 'src',
