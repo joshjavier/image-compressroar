b5ec1c100fcad39e45ea2a5007c38aaa3f44a477
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-07-19 14:45:26 +0800

    Process css and js bundles

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc"
 import esbuild from "./config/esbuild.js"
 import lightningcss from "./config/lightningcss.js"
+import processBundle from "./config/process-bundle.js"
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
@@ -10,6 +11,9 @@ export default function (eleventyConfig) {
   eleventyConfig.addPlugin(lightningcss)
   eleventyConfig.addPlugin(pluginWebc, {
     components: 'src/_components/**/*.webc',
+    bundlePluginOptions: {
+      transforms: [processBundle],
+    },
   })
 
   return {
