f7d3797b9ed77ac255ece72a8084078357691249
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-08-03 11:48:34 +0800

    Add validation logic to image card

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,15 +1,25 @@
 /**
- * Formats a file size into a human-readable string
- * @param {number} number - File size in bytes
- * @returns {string}
+ * Convert number of bytes into a human-readable format
+ *
+ * If unspecified, threshold defaults to 100, e.g., 100.1 KB becomes 100 KB.
+ * For our use case, if an image is >= 100 KB, the decimals don't matter, so
+ * we hide that information to reduce cognitive load for users.
+ *
+ * @param {number} bytes             - Number of bytes to convert
+ * @param {Object} options
+ * @param {number} options.precision - Number of digits after the decimal separator (default: 1)
+ * @param {number} options.threshold - Trim decimal when the integer part of the result is greater than or equal to this value (default: 100)
+ * @returns string
  */
-export function returnFileSize(number) {
-  if (number < 1e3) {
-    return `${number} bytes`
-  } else if (number >= 1e3 && number < 1e6) {
-    return `${(number / 1e3).toFixed(2)} KB`
-  } else {
-    return `${(number / 1e6).toFixed(2)} MB`
+export function bytesToSize(
+  bytes,
+  { precision = 1, threshold = 100} = {},
+) {
+  const units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  let l = 0, n = parseInt(bytes, 10) || 0
+  while (n >= 1024 && ++l) {
+    n = n / 1024
   }
+  return +n.toFixed(n < threshold && l > 0 ? precision : 0) + ' ' + units[l]
 }
 
