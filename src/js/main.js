423a2b709565dde9ac848fa45826c2c4d1d36c3d
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-08-06 07:34:54 +0800

    Implement downloading multiple images as a zip file

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
 import '@cloudfour/image-compare'
-import "./slider.js"
-import "./compress.js"
+// import "./slider.js"
+// import "./compress.js"
 
+import JSZip from 'jszip'
+import { saveAs } from "file-saver"
+import { bytesToSize } from "./_utils.js"
+
+// Make imports accessible in global scope
+window.JSZip = JSZip
+window.saveAs = saveAs
+window.bytesToSize = bytesToSize
